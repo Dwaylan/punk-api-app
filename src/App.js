8a1f0f6b47c087f84f458800ffb6012fdf7a283a
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-01-25 19:51:13 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Beer from "./components/Beer";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      beers: null,
+    };
+  }
+  componentDidMount() {
+    console.log("mounted");
+    fetch("https://api.punkapi.com/v2/beers")
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ beers: data });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          {" "}
+          {this.state.beers &&
+            this.state.beers.map((beer) => {
+              return (
+                <Beer
+                  name={beer.name}
+                  image={beer.image_url}
+                  tagline={beer.tagline}
+                  tips={beer.brewers_tips}
+                />
+              );
+            })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Beer/Beer.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Beer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Name</h1>
+        <img src="https://www.placecage.com/250/250" alt="beer images"></img>
+        <span> tagline ...</span>
+        <p>ipsum lorem</p>
+      </div>
+    );
+  }
+}
+
+export default Beer;
